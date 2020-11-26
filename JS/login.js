c3e02b1297aa515f3f4523f9e3865ec9c6c749e6
Bruna Brito <bruna.melo@bandtec.com.br>
2020-11-26 04:18:44 -0300

    paginas criadas

---
+++ JS/login.js
@@ -0,0 +1,8 @@
+function enviar_login(){
+    style.display.contactForm = 'none';
+    style.display.contactForm_cadastro = 'block';
+}
+function enviar_cadastro(){
+    style.display.contactForm = 'block';
+    style.display.contactForm_cadastro = 'none';
+}
