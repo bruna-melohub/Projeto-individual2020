faffa4c618dba5c1d08fd5ad732d257af7cfe024
Bruna Brito <bruna.melo@bandtec.com.br>
2020-11-27 02:37:32 -0300

    especies de cactos

--- JS/login.js
+++ JS/login.js
@@ -1,8 +1,49 @@
-function enviar_login(){
-    style.display.contactForm = 'none';
-    style.display.contactForm_cadastro = 'block';
+function cadastro() {
+    logar.style.display = 'none';
+    cadastro_container.style.display = 'block';
 }
-function enviar_cadastro(){
-    style.display.contactForm = 'block';
-    style.display.contactForm_cadastro = 'none';
+function entrar() {
+    aguardar();
+    var formulario = new URLSearchParams(new FormData(form_login));
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        body: formulario
+    }).then(resposta => {
+
+        if (resposta.ok) {
+
+            resposta.json().then(json => {
+
+                sessionStorage.login_usuario_meuapp = json.login;
+                sessionStorage.nome_usuario_meuapp = json.nome;
+
+                window.location.href = 'index.html';
+            });
+
+        } else {
+
+            console.log('Erro de login!');
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                finalizar_aguardar(texto);
+            });
+        }
+    });
+
+    return false;
 }
+
+function aguardar() {
+    btn_entrar.disabled = true;
+    img_aguarde.style.visibility = 'visible';
+    div_erro.style.visibility = 'hidden';
+}
+
+function finalizar_aguardar(resposta) {
+    btn_entrar.disabled = false;
+    img_aguarde.style.visibility = 'hidden';
+    div_erro.style.visibility = 'visible';
+    div_erro.innerHTML = resposta;
+}
+
